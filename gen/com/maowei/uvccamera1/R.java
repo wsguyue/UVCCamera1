87bbaad7131ba02d2ba84ef07790c8327699ef42
maowei <470419503@qq.com>
2016-01-02 22:21:50 +0800

    加入了so库文件和一些必要的java文件

--- gen/com/maowei/uvccamera1/R.java
+++ gen/com/maowei/uvccamera1/R.java
@@ -11,32 +11,45 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Example customization of dimensions originally defined in res/values/dimens.xml
          (such as screen margins) for screens with more than 820dp of available width. This
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050005;
+        public static final int button_size=0x7f050002;
+        public static final int horizontal_margin=0x7f050000;
+        public static final int list_font_size=0x7f050003;
+        public static final int list_height_min=0x7f050004;
+        public static final int vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int action_settings=0x7f090004;
+        public static final int name_text=0x7f090003;
+        public static final int spinner1=0x7f090002;
+        public static final int textView1=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int dialog_camera=0x7f030001;
+        public static final int listitem_device=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int camera=0x7f060005;
+        public static final int hello_world=0x7f060001;
+        public static final int no_device=0x7f060006;
+        public static final int refresh=0x7f060004;
+        public static final int select=0x7f060003;
     }
     public static final class style {
         /** 
@@ -60,10 +73,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int device_filter=0x7f040000;
     }
 }
